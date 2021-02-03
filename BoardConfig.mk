a6ce3400e009ea0c8d02bbb296fbf2bc4ae24d6d
Pranaya Deomani <deomanipranaya007@gmail.com>
2021-02-03 08:10:41 +0000

    curtana: Add kernel configuration
    Signed-off-by: Pranaya Deomani <deomanipranaya007@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,20 @@ TARGET_BOOTLOADER_BOARD_NAME := atoll
 TARGET_NO_BOOTLOADER := true
 TARGET_USES_UEFI := true
 
+# Kernel
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 earlycon=msm_geni_serial,0xa88000 loop.max_part=7 cgroup.memory=nokmem,nosocket androidboot.selinux=permissive androidboot.usbconfigfs=true
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_KERNEL_SECOND_OFFSET := 0x00f00000
+BOARD_DTB_OFFSET := 0x01f00000
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_BOOTIMG_HEADER_VERSION := 2
+TARGET_KERNEL_ARCH := arm64
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB) --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --second_offset $(BOARD_KERNEL_SECOND_OFFSET) --dtb_offset $(BOARD_DTB_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) --board $(TARGET_BOOTLOADER_BOARD_NAME) --pagesize $(BOARD_KERNEL_PAGESIZE) --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+
 # Platform
 PRODUCT_PLATFORM := atoll
 QCOM_BOARD_PLATFORMS += atoll
